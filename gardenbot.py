46fef7b1743c69f836951873e5e353f2e3a98e60
emishtal <info@mishtal.ru>
2025-05-05 19:44:10 +0300

    Add files via upload

---
+++ gardenbot.py
@@ -0,0 +1,16 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = "8196505151:AAEQA7KDP3APYJCJQ5ZBwEap1k_BjVJ_Dbo"
+WEBAPP_URL = "https://example.com"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        [InlineKeyboardButton("Открыть мини-апп", web_app=WebAppInfo(url=WEBAPP_URL))]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text("Нажми кнопку ниже:", reply_markup=reply_markup)
+
+app = Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.run_polling()
